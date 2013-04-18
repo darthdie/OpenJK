76eb8f5e6b933c122818169a25412fc60dabc0b6
Briar <briarbear7@gmail.com>
2013-04-18 11:49:50 -0500

    Added multisampling/AA

--- codemp/win32/win_glimp.cpp
+++ codemp/win32/win_glimp.cpp
@@ -26,6 +26,10 @@
 extern void WG_CheckHardwareGamma( void );
 extern void WG_RestoreGamma( void );
 
+int arbMultisampleFormat;
+bool arbMultisampleSupported = false;
+PIXELFORMATDESCRIPTOR lastPFD;
+
 typedef enum {
 	RSERR_OK,
 
@@ -343,7 +347,7 @@ static int GLW_MakeContext( PIXELFORMATDESCRIPTOR *pPFD )
 	// don't putz around with pixelformat if it's already set (e.g. this is a soft
 	// reset of the graphics system)
 	//
-	if ( !glw_state.pixelFormatSet )
+	if ( !glw_state.pixelFormatSet && !arbMultisampleSupported)
 	{
 		//
 		// choose, set, and describe our desired pixel format.  If we're
@@ -366,6 +370,14 @@ static int GLW_MakeContext( PIXELFORMATDESCRIPTOR *pPFD )
 		}
 
 		glw_state.pixelFormatSet = qtrue;
+	}
+	else if(arbMultisampleSupported) {
+		pixelformat = arbMultisampleFormat;
+		DescribePixelFormat( glw_state.hDC, pixelformat, sizeof( *pPFD ), pPFD );
+		if ( SetPixelFormat( glw_state.hDC, pixelformat, pPFD ) == FALSE ) {
+			Com_Printf ( "...SetPixelFormat failed\n", glw_state.hDC );
+			return TRY_PFD_FAIL_SOFT;
+		}
 	}
 
 	//
@@ -396,6 +408,79 @@ static int GLW_MakeContext( PIXELFORMATDESCRIPTOR *pPFD )
 	return TRY_PFD_SUCCESS;
 }
 
+bool InitMultisample(HINSTANCE hInstance,HWND hWnd,PIXELFORMATDESCRIPTOR pfd)
+{  
+	 // See If The String Exists In WGL!
+	/*if (!WGLisExtensionSupported("WGL_ARB_multisample"))
+	{
+		arbMultisampleSupported=false;
+		return false;
+	}*/
+
+	// Get Our Pixel Format
+	PFNWGLCHOOSEPIXELFORMATARBPROC wglChoosePixelFormatARB = (PFNWGLCHOOSEPIXELFORMATARBPROC)qwglGetProcAddress("wglChoosePixelFormatARB");	
+	if (!wglChoosePixelFormatARB) 
+	{
+		arbMultisampleSupported=false;
+		return false;
+	}
+
+	// Get Our Current Device Context
+	HDC hDC = GetDC(hWnd);
+
+	int		pixelFormat;
+	int		valid;
+	UINT	numFormats;
+	float	fAttributes[] = {0,0};
+
+	// These Attributes Are The Bits We Want To Test For In Our Sample
+	// Everything Is Pretty Standard, The Only One We Want To 
+	// Really Focus On Is The SAMPLE BUFFERS ARB And WGL SAMPLES
+	// These Two Are Going To Do The Main Testing For Whether Or Not
+	// We Support Multisampling On This Hardware.
+	int iAttributes[] =
+	{
+		//WGL_DRAW_TO_WINDOW_ARB,GL_TRUE,
+		0x2001, GL_TRUE,
+		WGL_SUPPORT_OPENGL_ARB,GL_TRUE,
+		//WGL_ACCELERATION_ARB,WGL_FULL_ACCELERATION_ARB,
+		0x2003,0x2027,
+		WGL_COLOR_BITS_ARB,24,
+		WGL_ALPHA_BITS_ARB,8,
+		WGL_DEPTH_BITS_ARB,16,
+		WGL_STENCIL_BITS_ARB,0,
+		WGL_DOUBLE_BUFFER_ARB,GL_TRUE,
+		//WGL_SAMPLE_BUFFERS_ARB,GL_TRUE,
+		0x2041, GL_TRUE,
+		//WGL_SAMPLES_ARB,4,
+		0x2042,4,
+		0,0
+	};
+
+	// First We Check To See If We Can Get A Pixel Format For 4 Samples
+	valid = wglChoosePixelFormatARB(hDC,iAttributes,fAttributes,1,&pixelFormat,&numFormats);
+ 
+	// If We Returned True, And Our Format Count Is Greater Than 1
+	if (valid && numFormats >= 1)
+	{
+		arbMultisampleSupported = true;
+		arbMultisampleFormat = pixelFormat;	
+		return arbMultisampleSupported;
+	}
+
+	// Our Pixel Format With 4 Samples Failed, Test For 2 Samples
+	iAttributes[19] = 2;
+	valid = wglChoosePixelFormatARB(hDC,iAttributes,fAttributes,1,&pixelFormat,&numFormats);
+	if (valid && numFormats >= 1)
+	{
+		arbMultisampleSupported = true;
+		arbMultisampleFormat = pixelFormat;	 
+		return arbMultisampleSupported;
+	}
+	  
+	// Return The Valid Format
+	return  arbMultisampleSupported;
+}
 
 /*
 ** GLW_InitDriver
@@ -523,6 +608,9 @@ static qboolean GLW_InitDriver( int colorbits )
 	glConfig.colorBits = ( int ) pfd.cColorBits;
 	glConfig.depthBits = ( int ) pfd.cDepthBits;
 	glConfig.stencilBits = ( int ) pfd.cStencilBits;
+	
+	//Multisampling
+	lastPFD = pfd;
 
 	return qtrue;
 }
@@ -641,7 +729,7 @@ static qboolean GLW_CreateWindow( int width, int height, int colorbits, qboolean
 		{
 			Com_Error (ERR_FATAL, "GLW_CreateWindow() - Couldn't create window");
 		}
-	
+
 		ShowWindow( tr.wv->hWnd, SW_SHOW );
 		UpdateWindow( tr.wv->hWnd );
 		Com_Printf ("...created window@%d,%d (%dx%d)\n", x, y, w, h );
@@ -658,6 +746,16 @@ static qboolean GLW_CreateWindow( int width, int height, int colorbits, qboolean
 		tr.wv->hWnd = NULL;
 
 		return qfalse;
+	}
+
+	if(!arbMultisampleSupported) {
+		if(InitMultisample(tr.wv->hInstance, tr.wv->hWnd, lastPFD)) {		
+			DestroyWindow (tr.wv->hWnd);
+			glw_state.pixelFormatSet = qfalse;
+			glw_state.hDC = NULL;
+			glw_state.hGLRC = 0;
+			return GLW_CreateWindow(width, height, colorbits, cdsFullscreen);
+		}
 	}
 
 	SetForegroundWindow( tr.wv->hWnd );
